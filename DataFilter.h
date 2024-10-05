03e00e518f8baa0992d8885721ab3f489c561a5f
Vhea He <98002993+vhea-h@users.noreply.github.com>
2024-10-05 00:05:14 -0400

    Add files via upload

---
+++ DataFilter.h
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include<stdio.h>
+#include<ctype.h>
+
+using namespace std;
+
+void filterData (string fileName);
+vector <string> removeIncompleteData(vector <string> strVec);
+vector <string> removeGarbledData(vector <string> strVec);
+vector <string> removeRepeatedData(vector <string> strVec);
